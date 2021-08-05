cdff402476f390f4423383bcbf4c17ab7706723a
jorgeasimas <jorgeasimas@hotmail.com>
2021-08-05 19:37:24 -0300

    saved

---
+++ src/component/text-component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './homepage.styles.css';
+
+const Text = ({text, author}) => {
+  return(
+  <div className="card-container-quote">
+    <h1><b>"{text}"</b></h1>
+    <p>{author}</p>
+  </div>
+
+
+  )}
+
+  export default Text;
+
+  
